92691b03ce21e359ae698e25ac7990a6e93d1841
Rohit <rohit8840185@gmail.com>
2025-04-13 18:15:11 +0530

    "/signup"

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -53,7 +53,7 @@ const SignUp = () => {
 
   if(response.data && response.data.accessToken){
     localStorage.setItem("token",response.data.accessToken)
-    navigate('/dashboard')
+    navigate('/login')
   }
 
 }  catch(error){
